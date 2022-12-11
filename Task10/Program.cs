4ac49d5e20601b84fbd7f1b728a1765688d1ae90
Lukas Kaufmann <kaufmlu1@fel.cvut.cz>
2022-12-11 23:26:42 +0100

    Task9 and 10

---
+++ Task10/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Task10
+{
+    class Program
+    {
+        public static void RenderValue(int value, int i)
+        {
+            if (value >= (i % 40)-1 && value <= (i % 40)+1)
+            {
+                Console.Write("#");
+            }
+            else
+            {
+                Console.Write(".");
+            }
+
+            if (i % 40 == 0)
+            {
+                Console.WriteLine();
+            }
+        }
+        static void Main()
+        {
+            int xRegister = 1;
+            int cycleCounter = 1;
+            RenderValue(xRegister,cycleCounter);
+            string[] rows = File.ReadAllLines("input.txt");
+            List<int> strengths = new List<int>();
+            foreach (var row in rows)
+            {
+                switch (row.Substring(0,4))
+                {
+                    case "noop":
+                        if ((cycleCounter - 20) % 40 == 0)
+                        {
+                            strengths.Add(xRegister * cycleCounter);
+                        }
+                        cycleCounter++;
+                        RenderValue(xRegister,cycleCounter);
+                        break;
+                    case "addx":
+                        if ((cycleCounter - 20) % 40 == 0)
+                        {
+                            strengths.Add(xRegister * cycleCounter);
+                        }
+                        cycleCounter++;
+                        RenderValue(xRegister,cycleCounter);
+                        if ((cycleCounter - 20) % 40 == 0)
+                        {
+                            strengths.Add(xRegister * cycleCounter);
+                        }
+                        
+                        cycleCounter++;
+                        RenderValue(xRegister,cycleCounter);
+                        xRegister += int.Parse(row.Substring(5));
+                        break;
+                }
+            }
+
+            Console.WriteLine(strengths.Sum());
+        }
+    }
+}
+
---
+++ Task9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace Task9
+{
+    class Program
+    {
+        static void Main()
+        {
+            Point headPoint = new Point(0, 0);
+            Point tailPoint = new Point(0, 0);
+            string[] rows = File.ReadAllLines("input.txt");
+            int i = 0;
+            
+            for (i = i; i < rows.Length; i++)
+            {
+                
+            }
+        }
+    }
+}
